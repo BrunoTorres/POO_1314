b1d17ebd542b272ba8b480ee31c925775fefc162
Bruno Pereira <bruno.torres.pereira@gmail.com>
2014-05-15 14:37:26 +0100

    Teste

--- Projecto POO/Fitness/src/FitnessUM.java
+++ Projecto POO/Fitness/src/FitnessUM.java
@@ -7,17 +7,17 @@ import java.util.TreeSet;
 public class FitnessUM
 {
     private DataBase db;
-    private Person person;
+   
     
+    // Gerenciamento da aplicação
     
-    
-    public boolean isAdmin(String email){                   // Procurar por email ou Admin admin?!
+    public boolean isAdmin(String email){                               // Procurar por email ou Admin admin?!
         TreeSet<Person> userList = (TreeSet)db.getEventList();
         boolean flag=false;
         boolean found=false;
     
        Iterator<Person> it=userList.iterator();
-       Person p = it.next();                              //DUVIDAS SE FUNCIONA POIS TENHO QUE ACEDER DUAS VEZES AO it.next()!!!!!
+       Person p = it.next();                                             //DUVIDAS SE FUNCIONA POIS TENHO QUE ACEDER DUAS VEZES AO it.next()!!!!!
        while(it.hasNext() && !found)
        {
            if((p.getEmail().equals(email)) && (p instanceof Admin))
@@ -42,7 +42,63 @@ public class FitnessUM
         
     }
     
-    public boolean removeUser(String email){
+    
+    public boolean addAdmin(String email,String pass,String name,char gender,GregorianCalendar date){
+        boolean flag= false;
+        Admin admin= new Admin(email,pass,name,gender,date);
+        flag=db.getUserListAdmin().add(admin);
+        
+        return flag;
+        
+    }
+    
+    /*
+    
+    ISTO È PARA O GIT
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    */
+    
+    //Propriedade dos Users
+    
+    public boolean addFriend(User u,String email){              //Adiciona amigos pelo email 
+       boolean flag=false,found=false;
+       Person friend=new User();
+        Iterator<Person> it=db.getUserList().iterator();
+       Person p = it.next();                                             //DUVIDAS SE FUNCIONA POIS TENHO QUE ACEDER DUAS VEZES AO it.next()!!!!!
+       while(it.hasNext() && !found)
+       {
+           if((p.getEmail().equals(email)) && (p instanceof Admin))
+           {   friend=p;
+               found=true;             
+           }      
+       }
+       u.getFriendsListAdmin().add(p.getName());
+       User u2=(User)friend;
+       flag=u2.getFriendsListAdmin().add(u.getName());              //Adiciona automaticamente um amigo ao outro 
+       
+       return flag;     
+    }
+    
+    
+    // Propriedade dos Administradores
+    
+    public boolean removeUser(String email){                               
         TreeSet<Person> userList = (TreeSet)db.getUserListAdmin();
         boolean Flag = false;
         for(Person u:userList){
@@ -53,7 +109,7 @@ public class FitnessUM
     }
     
     
-    public boolean removeActivity(Activity activity){
+    public boolean removeActivity(Activity activity){                       
         boolean Flag=false;
         TreeSet<Person> userList=(TreeSet<Person>)db.getUserListAdmin();
         
