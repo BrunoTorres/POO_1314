eecf0c9153fd86e4dad13e04973d596a0c1da06f
Bruno Pereira <bruno.torres.pereira@gmail.com>
2014-05-15 12:21:20 +0100

    poo

--- Projecto POO/Fitness/src/AdminRightsDB.java
+++ Projecto POO/Fitness/src/AdminRightsDB.java
@@ -1,8 +1,28 @@
 
+import java.util.TreeSet;
+
+
 public class AdminRightsDB extends DataBase
 {
+    
+    
+    
     public boolean removeActivity(Activity activity){
+        boolean Flag=true;
+        TreeSet<Person> userList=(TreeSet<Person>)super.getUserListAdmin();
+        
+        for(Person p:userList){
+            User u= (User)p;
+            TreeSet<ActivityList> userActivities= (TreeSet)u.getUserActivitiesAdmin();         
+            TreeSet<Activity> activityList=userActivities.getActivityListAdmin();
+            
+            Flag=activityList.remove(activity);           
+                       
+        }
+            return Flag;
+        
+        
        // return super.activitiesList.remove(activity);
-        return false;
+
     }
 }
--- Projecto POO/Fitness/src/DataBase.java
+++ Projecto POO/Fitness/src/DataBase.java
@@ -1,3 +1,4 @@
+import java.util.Set;
 import java.util.TreeSet;
 
 public class DataBase
@@ -6,5 +7,63 @@ public class DataBase
     private TreeSet<Person> userList;
     private TreeSet<Event> eventList;
     
+    
+    public DataBase(){
+        this.sportsType= new TreeSet<Sport>();
+        this.userList= new TreeSet<Person>();
+        this.eventList= new TreeSet<Event>();
+    }
+    public DataBase(TreeSet<Sport> sports,TreeSet<Person> user,TreeSet<Event> eventList){        
+        for(Sport s:sports)
+            this.sportsType.add(s.clone());
+        for(Person p:user)
+            this.userList.add((User)p.clone());
+        for(Event e :eventList)
+            this.eventList.add(e.clone());         
+               
+            
+    }
+    
+    
+    public DataBase(DataBase db){
+        this.sportsType=(TreeSet<Sport>)db.getSportsType();
+        this.userList=(TreeSet<Person>)db.getUserList();
+        this.eventList=(TreeSet<Event>)db.getEventList();
+    }
+    
+    public Set<Sport> getSportsType(){
+        TreeSet<Sport> aux=new TreeSet<Sport>();
+        for(Sport s:this.sportsType)
+            aux.add(s.clone());
+        return aux;
+    }
+    
+    public Set<Person> getUserList(){
+        TreeSet<Person> aux = new TreeSet<Person>();
+        for(Person p : this.userList)
+            aux.add((User)p.clone());
+        return aux;
+    }
+    
+    public Set<Event> getEventList(){
+        TreeSet<Event> aux = new TreeSet<Event>();
+        for(Event e: this.eventList)
+            aux.add(e.clone());
+    }
+    
+    
+    // to AdminRights
+    
+    public Set<Sport> getSportsTypeAdmin(){
+        return this.sportsType;
+    }
+    
+   public Set<Person> getUserListAdmin(){
+       return this.getUserList();
+   }
+   
+   public Set<Event> getEventListAdmin(){
+       return this.eventList;
+   }
     
 }
