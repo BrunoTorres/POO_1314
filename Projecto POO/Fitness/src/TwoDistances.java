e84052a16dd4150f7924293658a8e2579cdf8c77
João Mano <joaomanooo@gmail.com>
2014-05-17 13:55:16 +0100

    TwoDistances -> Getters, setters, equals, toString e clone

--- Projecto POO/Fitness/src/TwoDistances.java
+++ Projecto POO/Fitness/src/TwoDistances.java
@@ -1,5 +1,77 @@
 
-public class TwoDistances extends Distance
-{
-    private float secondDistance; // distancia vertical (Kilómetros)
+import java.util.GregorianCalendar;
+
+
+public class TwoDistances extends Distance {
+
+	private float secondDistance; // distancia vertical (Kilómetros)
+	
+	public TwoDistances(){
+		super();
+		this.secondDistance = 0;
+	}
+	
+	public TwoDistances(String sportName, String name, GregorianCalendar date, GregorianCalendar timeSpent, int calories, String weather, float distance, float secondDistance){
+		super(sportName, name, date, timeSpent, calories, weather, distance);
+		this.secondDistance = secondDistance;
+	}
+	
+	public TwoDistances(TwoDistances twoDist){
+		super(twoDist);
+		this.secondDistance = twoDist.getSecondDistance();
+	}
+
+	public float getSecondDistance() {
+		return this.secondDistance;
+	}
+	
+	public void setSecondDistance(float secondDistance){
+		this.secondDistance = secondDistance;
+	}
+	
+	@Override
+	public void setCalories() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+	
+	@Override
+	public void setRecords() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public boolean equals(Object a) {
+		if(this == a)
+			return true;
+		if(a == null || this.getClass() != a.getClass())
+			return false;
+		
+		TwoDistances twoDist = (TwoDistances) a;		
+		
+		return (super.equals(twoDist) &&
+			    this.secondDistance == twoDist.getSecondDistance());
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 41 * hash + Float.floatToIntBits(this.secondDistance);
+		return hash;
+	}
+	
+	@Override
+	public TwoDistances clone(){
+		return new TwoDistances(this);
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append("Altitude: "); // Não sei bem o que escrever aqui xD
+		sb.append(this.getSecondDistance()).append("\n");
+		
+		return sb.toString();
+	}
+
 }
