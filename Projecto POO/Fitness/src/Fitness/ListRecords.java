ce9c3d22eaca2cda21fdfb100265873628fa94c0
Bruno Pereira <bruno.torres.pereira@gmail.com>
2014-05-23 11:34:35 +0100

    new

---
+++ Projecto POO/Fitness/src/Fitness/ListRecords.java
@@ -0,0 +1,74 @@
+
+package Fitness;
+
+import java.util.TreeSet;
+
+public class ListRecords {
+    private final String name;            // nome do tipo (Cycling...)
+    private TreeSet<Records> recs;
+    
+public ListRecords(){
+    this.name="";
+    this.recs=new TreeSet();
+}
+public ListRecords(String name){
+    this.name=name;
+    this.recs=new TreeSet();
+}
+
+public ListRecords(String name, TreeSet<Records> recs){
+    this.name=name;
+    for(Records r:recs)
+        this.recs.add(r.clone());
+    }
+public ListRecords(ListRecords list){
+    this.name=list.getName();
+    this.recs=list.getList();
+}
+
+    private TreeSet<Records> getList() {
+        TreeSet<Records> aux=new TreeSet();
+        for(Records r:this.recs)
+            aux.add(r);
+        return aux;
+   
+    }
+
+    private String getName() {
+        return this.name;        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //toString,equals,clone
+    
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Nome: ").append(this.name).append("\n");
+        for(Records r:this.recs)
+            sb.append((r.toString()));
+        
+        return sb.toString();
+    }
+    
+    public boolean equals(Object o){
+        if(this==o)
+            return true;
+        if((o==null) ||(o.getClass() != this.getClass()))
+            return false;
+        ListRecords list=(ListRecords)o;
+        return (this.name==list.getName() &&
+                this.recs.equals(list.getList()));
+    }
+    
+    public ListRecords clone(){
+        return new ListRecords(this);
+    }
+    
+}
