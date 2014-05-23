6c88ccc0f1f1b5759a70d81faf43a209225de8e0
João Mano <joaomanooo@gmail.com>
2014-05-23 14:12:02 +0100

    Revert "menu"
    This reverts commit 65c6c097da6d4bd19471344d1afd964daa4a1aa3.

--- Projecto POO/Fitness/src/Fitness/Person.java
+++ Projecto POO/Fitness/src/Fitness/Person.java
@@ -1,7 +1,6 @@
 package Fitness;
 
 
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Objects;
 
@@ -91,11 +90,7 @@ public abstract class Person
     sb.append("Email: ");sb.append(this.email).append("\n");
     sb.append("Password: ");sb.append(this.password).append("\n");
     sb.append("Gender: ");sb.append(this.gender).append("\n");
-    sb.append("Date of Birth: ");
-    sb.append("Data: ");
-    sb.append(this.dateOfBirth.get(Calendar.DAY_OF_MONTH)).append(" / ");;
-    sb.append(this.dateOfBirth.get(Calendar.MONTH)).append(" / ");
-    sb.append(this.dateOfBirth.get(Calendar.YEAR)).append("\n"); 
+    sb.append("Date of Birth: ");sb.append(this.dateOfBirth.toString()).append("\n");
     
     return sb.toString(); 
 }
