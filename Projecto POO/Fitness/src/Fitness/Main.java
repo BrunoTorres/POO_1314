e5d98e4d72211139a8e9ec7142527c0eaa85a3fb
Miguel <miguelguimaraess@hotmail.com>
2014-05-27 18:15:15 +0100

    cenas

--- Projecto POO/Fitness/src/Fitness/Main.java
+++ Projecto POO/Fitness/src/Fitness/Main.java
@@ -79,27 +79,21 @@ public class Main {
        //Activity act2=um.createActivity("Running");
        GregorianCalendar date1=new GregorianCalendar(2000,11,2);
        GregorianCalendar date2= new GregorianCalendar(2001,9,3);
-       GregorianCalendar date3= new GregorianCalendar(2002,10,20);
-       GregorianCalendar date4= new GregorianCalendar(2002,11,4);
-       GregorianCalendar date5= new GregorianCalendar(2013,10,4);
-       GregorianCalendar date6= new GregorianCalendar(2002,10,30);
-       GregorianCalendar date7= new GregorianCalendar(2002,10,4);
-
-        Activity act1 = new MountainBiking("bike", date1, 155, 20, 5, "chuva");
-        Activity act2 = new Cycling("cy", date2, 155, 20, "chuva");
-        Activity act3 = new Orienteering("ah e tal", date3, 155, 20, "sol");
-        Activity act4 = new Sailling("regata", date4, 155, 20, "tempestade do caralho");
-        Activity act5 = new Running("run", date5, 30, 5, "sole");
-        Activity act6 = new Running("corrida com os amigos", date6, 66, 15, "chuva");
-       
+       GregorianCalendar date3= new GregorianCalendar(2002,10,4);
+       /*
+        Activity act = new MountainBiking("bike", date1, 155, 20, 5, "chuva");
+        Activity act1 = new Cycling("cy", date2, 155, 20, "chuva");
+        Activity act2 = new Running("run", date3, 155, 20, "chuva");
+       */
+        Activity act = new Running("run", date3, 100, 100000, "chuva");
+        Activity act1 = new Running("run", date3, 10, 50000, "chuva");
+        Activity act2 = new Running("run", date3, 155, 20000, "chuva");
        //Activity act3=um.createActivity("Swimming");       
-       u.addActivity(act1, "MountainBiking");
-       u.addActivity(act2, "Cycling");
-       u.addActivity(act3, "Orienteering");
-       u.addActivity(act4, "Sailling");
-       u.addActivity(act5, "Running");
-       u.addActivity(act6, "Running");
-
+       u.addActivity(act, "Running");
+       u.addActivity(act1, "Running");
+       u.addActivity(act2, "Running");
+       
+       
       // TreeSet activi=(TreeSet)um.getLast10Activities(u);
       //System.out.println(activi.toString()); 
       
@@ -113,8 +107,7 @@ public class Main {
            um.acceptFriend(u,"user3@gmail.com");
        }
        for(String s :u.getRecords().keySet())
-          System.out.println(u.getRecords().get(s).toString());
-
+       System.out.println(u.getRecords().get(s).toString());
        //System.out.println(u.getFriendsList().toString());
        
        //Para ver actividades do amigo= list amigos getUserbyEmail depois getLast10activity do user e print
